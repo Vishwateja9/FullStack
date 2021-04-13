8011c7db8397b33c4b3cc2413b19fb306cd899ef
91798 <amirishettiviswateja.com>
2021-04-13 16:18:30 +0530

    singup backend

--- src/main/java/com/fullstackproject/SignUpBackEnd/SignUpBackEndApplication.java
+++ src/main/java/com/fullstackproject/SignUpBackEnd/SignUpBackEndApplication.java
@@ -2,6 +2,8 @@ package com.fullstackproject.SignUpBackEnd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SignUpBackEndApplication {
@@ -10,4 +12,9 @@ public class SignUpBackEndApplication {
 		SpringApplication.run(SignUpBackEndApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate resttemplate() {
+		return new RestTemplate();
+	}
 }
+
